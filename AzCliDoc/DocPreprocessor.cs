c341e3bfe7b567ae35bf041fe4bcdc442778ea2d
Erich Wang <eriwan@microsoft.com>
2016-08-10 16:51:20 +0800

    update nuget package

--- AzCliDoc/DocPreprocessor.cs
+++ AzCliDoc/DocPreprocessor.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.DocAsCode.EntityModel.Plugins.OpenPublishing.AzureCli.Model;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Text;
