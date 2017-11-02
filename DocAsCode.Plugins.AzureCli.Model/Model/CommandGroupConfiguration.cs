516166db07d20962f91710c5d3cc1411e703bdfb
VSC CI Service <vscavu@microsoft.com>
2017-11-02 17:01:37 +0800

    Update configurations for processor

---
+++ DocAsCode.Plugins.AzureCli.Model/Model/CommandGroupConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.DocAsCode.EntityModel.Plugins.OpenPublishing.AzureCli
+{
+    public class CommandGroupConfiguration
+    {
+        public string CommandPrefix { get; set; }
+
+        public string Language { get; set; }
+    }
+
+    public enum CommandGroupType
+    {
+        AZURE,
+
+        VSTS
+    }
+}
